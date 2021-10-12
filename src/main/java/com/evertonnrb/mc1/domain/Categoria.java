6081c5b887e9b3ddfdf89b4b3409e86afaa5a46f
Everton Ribeiro <everton@debian.everton>
2021-10-12 15:14:40 -0400

    adicionados relacionamentos many to many produtos/categorias

--- src/main/java/com/evertonnrb/mc1/domain/Categoria.java
+++ src/main/java/com/evertonnrb/mc1/domain/Categoria.java
@@ -1,18 +1,20 @@
 package com.evertonnrb.mc1.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 public class Categoria implements Serializable {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -37,6 +39,14 @@ public class Categoria implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
---
+++ src/main/java/com/evertonnrb/mc1/repositories/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.evertonnrb.mc1.repositories;
+
+import com.evertonnrb.mc1.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+}
