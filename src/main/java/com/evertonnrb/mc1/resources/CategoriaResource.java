c7796967b28a9667986d11b9da18a5e58b85364f
Everton Ribeiro <everton@debian.everton>
2021-10-14 15:32:35 -0400

    listndo categorias

--- src/main/java/com/evertonnrb/mc1/resources/CategoriaResource.java
+++ src/main/java/com/evertonnrb/mc1/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.evertonnrb.mc1.resources;
 
 import com.evertonnrb.mc1.domain.Categoria;
+import com.evertonnrb.mc1.domain.dto.CategoriaDTO;
 import com.evertonnrb.mc1.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -43,5 +46,14 @@ public class CategoriaResource {
     public ResponseEntity<?> delete(@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> dtoList = list.stream()
+                .map(CategoriaDTO::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtoList);
     }
 }
