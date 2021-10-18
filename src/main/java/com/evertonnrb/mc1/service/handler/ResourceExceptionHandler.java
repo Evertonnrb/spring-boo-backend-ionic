bb079cad41091ecc1b6ddec85febfbf8d95a89c3
Everton Ribeiro <everton@debian.everton>
2021-10-18 12:39:54 -0400

    personalizando o retorna da validação

--- src/main/java/com/evertonnrb/mc1/service/handler/ResourceExceptionHandler.java
+++ src/main/java/com/evertonnrb/mc1/service/handler/ResourceExceptionHandler.java
@@ -2,8 +2,11 @@ package com.evertonnrb.mc1.service.handler;
 
 import com.evertonnrb.mc1.service.exceptions.DataIntegrityException;
 import com.evertonnrb.mc1.service.exceptions.ObjectNotFoundException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,15 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandartError> dataIntegrityException(DataIntegrityException e, HttpServletRequest req){
         StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandartError> validation(MethodArgumentNotValidException e, HttpServletRequest req){
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),e.getMessage(),System.currentTimeMillis());
+        for(FieldError x :  e.getBindingResult().getFieldErrors()){
+            err.addError(x.getField() ,x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
